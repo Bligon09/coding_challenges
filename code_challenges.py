d754d9dd917c1c171ee245836f5b23511b145cf5
Brittany Ligon <bligon09@gmail.com>
2023-08-16 12:47:41 -0700

    descending number

--- code_challenges.py
+++ code_challenges.py
@@ -7,13 +7,32 @@
 
 # print(remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
-def filter_list(l):
-    'return a new list with the strings filtered out'
-    new_list=[]
-    for element, i in  enumerate(l):
-        if isinstance(i, int)== True:
-            new_list.append(i)
+# def filter_list(l):
+#     'return a new list with the strings filtered out'
+#     new_list=[]
+#     for element, i in  enumerate(l):
+#         if isinstance(i, int)== True:
+#             new_list.append(i)
 
-    return new_list
+#     return new_list
 
-print(filter_list([1,'a','b',0,15]))
+# print(filter_list([1,'a','b',0,15]))
+
+def descending_order(num):
+    # Bust a move right here
+    # take a non-negative number, and return it with its digits in descending order
+    # example: 12345 should return 54321
+    num_list=[]
+    num_str=str(num)
+    new_str=""
+    
+    for element in num_str:
+        num_list.append(element)
+        
+    num_list.sort()
+    num_list.reverse()
+    
+    for num in num_list:
+        new_str+=num
+        
+    return(int(new_str))
