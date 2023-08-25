dd04fce3980a3d18fc01a67bc3ec20080c91a93b
Brittany Ligon <bligon09@gmail.com>
2023-08-24 21:05:18 -0700

    banjo problem

--- code_challenges.py
+++ code_challenges.py
@@ -18,21 +18,33 @@
 
 # print(filter_list([1,'a','b',0,15]))
 
-def descending_order(num):
-    # Bust a move right here
-    # take a non-negative number, and return it with its digits in descending order
-    # example: 12345 should return 54321
-    num_list=[]
-    num_str=str(num)
-    new_str=""
+# def descending_order(num):
+#     # Bust a move right here
+#     # take a non-negative number, and return it with its digits in descending order
+#     # example: 12345 should return 54321
+#     num_list=[]
+#     num_str=str(num)
+#     new_str=""
     
-    for element in num_str:
-        num_list.append(element)
+#     for element in num_str:
+#         num_list.append(element)
         
-    num_list.sort()
-    num_list.reverse()
+#     num_list.sort()
+#     num_list.reverse()
     
-    for num in num_list:
-        new_str+=num
+#     for num in num_list:
+#         new_str+=num
         
-    return(int(new_str))
+#     return(int(new_str))
+
+def areYouPlayingBanjo(name):
+    # Create a function which answers the question "Are you playing banjo?".
+    # If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+
+    cap_name=name.upper()
+
+    if cap_name.startswith('R')==True:
+        return name + ' plays banjo'
+    else:
+        return name + ' does not play banjo'
